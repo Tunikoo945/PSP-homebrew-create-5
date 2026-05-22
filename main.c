0b018c5ad804a1a180883c6a3ad9c3633fe9d26f
Tunikoo945 <coderkodular@hotmail.com>
2026-05-22 17:29:55 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,65 +1,33 @@
 #include <pspkernel.h>
 #include <pspdebug.h>
+#include <pspctrl.h>
 
 PSP_MODULE_INFO("HelloPSP", 0, 1, 0);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
 
-// Programdan çıkış fonksiyonu
-int exit_callback(int arg1, int arg2, void *common)
-{
-    sceKernelExitGame();
-    return 0;
-}
-
-// Callback thread
-int callback_thread(SceSize args, void *argp)
-{
-    int cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
-    sceKernelRegisterExitCallback(cbid);
-    sceKernelSleepThreadCB();
-    return 0;
-}
-
-// Callback başlat
-int setup_callbacks(void)
-{
-    int thid = sceKernelCreateThread(
-        "update_thread",
-        callback_thread,
-        0x11,
-        0xFA0,
-        0,
-        0
-    );
-
-    if (thid >= 0)
-        sceKernelStartThread(thid, 0, 0);
-
-    return thid;
-}
-
 int main()
 {
-    setup_callbacks();
-
-    // Ekranı başlat
     pspDebugScreenInit();
+    sceCtrlSetSamplingCycle(0);
+    sceCtrlSetSamplingMode(PSP_CTRL_MODE_ANALOG);
 
-    // Yazı yaz
-    pspDebugScreenPrintf("Merhaba PSP!\n");
-    pspDebugScreenPrintf("start tusuna basip cikabilirsiniz\n");
+    SceCtrlData pad;
 
-    // Sonsuz döngü
+    pspDebugScreenPrintf("Merhaba PSP!
+");
+    pspDebugScreenPrintf("Cikmak icin START tusuna bas.
+");
+
     while (1)
     {
-        sceKernelDelayThread(10000);
+        sceCtrlReadBufferPositive(&pad, 1);
+
+        if (pad.Buttons & PSP_CTRL_START)
+        {
+            break;
+        }
     }
 
-    if(pad.Buttons & PSP_CTRL_START)
-       {
-          sceKernelExitGame()
-
-}
-
+    sceKernelExitGame();
     return 0;
 }
